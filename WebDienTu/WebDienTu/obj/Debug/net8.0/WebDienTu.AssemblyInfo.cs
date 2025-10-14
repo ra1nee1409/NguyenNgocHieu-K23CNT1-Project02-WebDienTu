245f23aae1b933dbed48bcdbd31e2837c6004537
ra1nee1409 <sthieu14092005@gmail.com>
2025-10-14 21:26:00 +0700

    update

--- WebDienTu/WebDienTu/obj/Debug/net8.0/WebDienTu.AssemblyInfo.cs
+++ WebDienTu/WebDienTu/obj/Debug/net8.0/WebDienTu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebDienTu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d84d747b82a01072e92339cb3983082ee78976e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47d2709629a5c13324010b25f68144cc9e97cf08")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebDienTu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebDienTu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
